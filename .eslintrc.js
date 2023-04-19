a7a29924516df694fd26978c46aad5c59d5e452d
xollee <devleexol@gmail.com>
2023-04-20 01:33:55 +0900

    chore: ESLint, Prettier 설정

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: [
+    "plugin:react/recommended",
+    "airbnb",
+    "plugin:prettier/recommended",
+  ],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+  },
+  plugins: ["react", "prettier"],
+  rules: {
+    quotes: ["error", "double"],
+    "no-unused-vars": "warn",
+    "react/react-in-jsx-scope": "off",
+    "react/jsx-filename-extension": [1, { extensions: [".js", ".jsx"] }],
+    "react/style-prop-object": "off",
+    "react/prop-types": "warn",
+    "react/no-unstable-nested-components": "warn",
+    "react/jsx-props-no-spreading": "off",
+    "no-underscore-dangle": "off",
+    "import/no-extraneous-dependencies": "off",
+    "no-use-before-define": "warn",
+  },
+};
